ae8ab69bc2ae8ce09971d31a1d256fafdfeab883
Andrew Gaus <andrewgaus815@gmail.com>
2017-07-25 16:03:18 -0400

    Initial Commit for Matlab/Octave Model of game, for statistical analysis

---
+++ MatlabModel/wrapper.m
@@ -0,0 +1,18 @@
+clear all;
+close all;
+
+%% this function will call main.m by providing badgemons and femme
+
+% load badgers and femmes
+
+% setup percentages for counter and special attack, and percentage that badger would succesffully counter attack
+
+% enable whether or not badger will choose random attacks or try to special attack each time
+% or if badger learns
+
+
+%% loop over everything
+% badger
+  % femme
+    % percentages
+      % call main.m    
