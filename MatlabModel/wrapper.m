60a547d517a835b18e05c85b75b542eb51e8452b
Andrew Gaus <andrewgaus815@gmail.com>
2017-07-25 21:49:16 -0400

    Added number of iterations as an output row

--- MatlabModel/wrapper.m
+++ MatlabModel/wrapper.m
@@ -9,7 +9,8 @@ DataOut{rowIdx,4} = 'Counter Success Chance';
 % DataOut{rowIdx,4} = 'Badger Special Used Percentage';
 % DataOut{rowIdx,4} = 'Femme Special Used Percentage';
 % DataOut{rowIdx,4} = 'No Special Used Percentage';
-DataOut{rowIdx,5} = 'Badger Win Percentage';
+DataOut{rowIdx,5} = 'Number of Runs';
+DataOut{rowIdx,6} = 'Badger Win Percentage';
 
 rowIdx = rowIdx + 1;
 
@@ -66,7 +67,8 @@ for badgerIdx = 1:length(badgerTest)
 				DataOut{rowIdx,2} = femmeTest(femmeIdx).name;
 				DataOut{rowIdx,3} = CounterAttackChancePercentage(caPercIdx);
 				DataOut{rowIdx,4} = CounterAttackChanceSuccessPercentage(caSuccIdx);
-				DataOut{rowIdx,5} = length(find(results == 1))/length(results) * 100;
+				DataOut{rowIdx,5} = NumRunsPerSetOfInputs;
+				DataOut{rowIdx,6} = length(find(results == 1))/length(results) * 100;
 				
 				rowIdx = rowIdx + 1;
 				
@@ -76,9 +78,9 @@ for badgerIdx = 1:length(badgerTest)
 end
 
 % write output file
-fid = fopen ('testingFile.csv', 'wt');
-fprintf (fid,'%s, %s, %s, %s, %s\n', DataOut{1,:})
+fid = fopen ('TestResults.csv', 'wt');
+fprintf (fid,'%s, %s, %s, %s, %s, %s\n', DataOut{1,:})
 for ii = 2:rowIdx-1
-	fprintf (fid,'%s, %s, %f, %f, %f', DataOut{ii,:})
+	fprintf (fid,'%s, %s, %f, %f, %f, %f', DataOut{ii,:})
 end
 fclose (fid);
